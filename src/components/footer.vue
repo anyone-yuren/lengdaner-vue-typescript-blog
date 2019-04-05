d0ba5705d9f388e17791a958bb7424650aa60ea1
lengdancome <zhang1471142227@163.com>
2019-04-05 16:03:18 +0800

    增加前端展示代码

---
+++ src/components/footer.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="footer"> 学习养成记 ©2018 Created by lengdaner </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Footer extends Vue {}
+</script>
+<style scoped>
+.footer {
+  text-align: center;
+  padding: 20px;
+  font-weight: bold;
+}
+</style>
+
