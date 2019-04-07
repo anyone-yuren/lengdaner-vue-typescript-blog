8324fd058db4db4d98553f6e5a3faee92a13f1ca
lengdancome <zhang1471142227@163.com>
2019-04-07 15:41:30 +0800

    更改了对列表页面的调整

--- src/views/articleDetail.vue
+++ src/views/articleDetail.vue
@@ -1,90 +1,84 @@
 <template>
   <div style="width: 100%">
     <div class="article clearfix">
-      <div v-show="!isLoading"
-           :style="{'width': isMobileOrPc ? '100%' : '75%'}"
-           class="article-left fl">
+      <div
+        v-show="!isLoading"
+        :style="{'width': isMobileOrPc ? '100%' : '75%'}"
+        class="article-left fl"
+      >
         <div class="header">
           <h1 class="title">{{articleDetail.title}}</h1>
           <div class="author">
             <div class="avatar">
-              <img class="auth-logo"
-                   src="../assets/userLogo.jpeg"
-                   alt="lengdaner">
+              <img class="auth-logo" src="../assets/userLogo.jpeg" alt="lengdaner">
             </div>
             <div class="info">
               <span class="name">
                 <span>{{articleDetail.author}}</span>
               </span>
-              <div props-data-classes="user-follow-button-header"
-                   data-author-follow-button="" />
+              <div props-data-classes="user-follow-button-header" data-author-follow-button/>
               <div class="meta">
-                <span class="publish-time">
-                  {{articleDetail.create_time? formatTime(articleDetail.create_time): ''}}
-                </span>
-                <span class="wordage">
-                  字数 {{articleDetail.numbers}}
-                </span>
-                <span class="views-count">
-                  阅读 {{articleDetail.meta.views}}
-                </span>
-                <span class="comments-count">
-                  评论 {{articleDetail.meta.comments}}
-                </span>
-                <span class="likes-count">
-                  喜欢 {{articleDetail.meta.likes}}
-                </span>
+                <span
+                  class="publish-time"
+                >{{articleDetail.create_time? formatTime(articleDetail.create_time): ''}}</span>
+                <span class="wordage">字数 {{articleDetail.numbers}}</span>
+                <span class="views-count">阅读 {{articleDetail.meta.views}}</span>
+                <span class="comments-count">评论 {{articleDetail.meta.comments}}</span>
+                <span class="likes-count">喜欢 {{articleDetail.meta.likes}}</span>
               </div>
             </div>
-            <div class="tags "
-                 title="标签">
-              <el-tag size="mini"
-                      v-for="tag in articleDetail.tags"
-                      :key="tag._id"
-                      class="tag"
-                      type="success">{{tag.name}}</el-tag>
+            <div class="tags" title="标签">
+              <el-tag
+                size="mini"
+                v-for="tag in articleDetail.tags"
+                :key="tag._id"
+                class="tag"
+                type="success"
+              >{{tag.name}}</el-tag>
             </div>
-            <span class="clearfix" />
+            <span class="clearfix"/>
           </div>
         </div>
         <div class="content">
-          <div><img :src="articleDetail.img_url" alt=""></div>
-          <div id="content"
-               class="article-detail"
-               v-html="articleDetail.content">
+          <div>
+            <img :src="articleDetail.img_url" alt>
           </div>
+          <div id="content" class="article-detail" v-html="articleDetail.content"></div>
         </div>
         <div class="heart">
-          <el-button type="danger"
-                     size="large"
-                     icon="heart"
-                     :loading="isLoading"
-                     @click="likeArticle">
-            点赞
-          </el-button>
+          <el-button
+            type="danger"
+            size="large"
+            icon="heart"
+            :loading="isLoading"
+            @click="likeArticle"
+          >点赞</el-button>
         </div>
         <div class="comment">
-          <el-input placeholder="文明社会，理性评论"
-                    type="textarea"
-                    v-model="content"></el-input>
-          <el-button style="margin-top: 15px"
-                     type="primary"
-                     :loading="btnLoading"
-                     @click="handleAddComment">发 送</el-button>
+          <el-input placeholder="文明社会，理性评论" type="textarea" v-model="content"></el-input>
+          <el-button
+            style="margin-top: 15px"
+            type="primary"
+            :loading="btnLoading"
+            @click="handleAddComment"
+          >发 送</el-button>
         </div>
-        <CommentList v-if="!isLoading"
-                     :numbers="articleDetail.meta.comments"
-                     :list="articleDetail.comments"
-                     :article_id="articleDetail._id"
-                     @refreshArticle="refreshArticle" />
+        <CommentList
+          v-if="!isLoading"
+          :numbers="articleDetail.meta.comments"
+          :list="articleDetail.comments"
+          :article_id="articleDetail._id"
+          @refreshArticle="refreshArticle"
+        />
       </div>
-      <div v-if="!isMobileOrPc"
-           style="width: 23%"
-           class="article-right fr anchor"
-           v-html="articleDetail.toc"></div>
+      <div
+        v-if="!isMobileOrPc"
+        style="width: 23%"
+        class="article-right fr anchor"
+        v-html="articleDetail.toc"
+      ></div>
       <LoadingCustom v-if="isLoading"></LoadingCustom>
     </div>
-
   </div>
 </template>
 <script lang="ts">
@@ -123,7 +117,7 @@ export default class ArticleDetail extends Vue {
   articleDetail: any = {
     toc: "",
     _id: "",
-    author: "lenger",
+    author: "lengdaner",
     category: [],
     comments: [],
     create_time: "",
@@ -143,7 +137,7 @@ export default class ArticleDetail extends Vue {
 
   mounted() {
     this.params.id = this.$route.query.article_id;
-    this.params.id = "5ca86fc99e89ab62a3e77fcf";
+    // this.params.id = "5ca86fc99e89ab62a3e77fcf";
     if (this.$route.path === "/about") {
       this.params.type = 3;
     }
