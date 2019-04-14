6211047429e69554a17b5ceeb1ebad96f5cc25c0
lengdancome <zhang1471142227@163.com>
2019-04-14 09:21:36 +0800

    更改了github登陆

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,14 @@
 const config = {
-  'oauth_uri': 'https://github.com/login/oauth/authorize',
-  'redirect_uri': 'https://biaochenxuying.cn/login', // 请修改成你的信息
-  'client_id': 'XXXXXXXXX', // 请修改成你的信息
-  'client_secret': 'XXXXXXXXX', // 请修改成你的信息
+  oauth_uri: 'https://github.com/login/oauth/authorize',
+  redirect_uri: 'http://www.ityouknow.top/login', // 请修改成你的信息
+  client_id: '6d7e75e15622dc214c11', // 请修改成你的信息
+  client_secret: '4f4821b8842b8feb38e39975b653f7f122f4fb71' // 请修改成你的信息
 };
 
 // 本地开发环境下 （如下的信息在本地开发可以当测试用）
 if (process.env.NODE_ENV === 'development') {
-  config.redirect_uri = "http://localhost:3001/login"
-  config.client_id = "502176cec65773057a9e"
-  config.client_secret = "65d444de381a026301a2c7cffb6952b9a86ac235"
+  config.redirect_uri = 'http://localhost:3001/login';
+  config.client_id = '6d7e75e15622dc214c11';
+  config.client_secret = '4f4821b8842b8feb38e39975b653f7f122f4fb71';
 }
 export default config;
