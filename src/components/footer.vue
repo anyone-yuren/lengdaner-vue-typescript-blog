111ebbe3f89ad75ce39cf6f85014a5cc6711fd2f
lengdancome <zhang1471142227@163.com>
2019-04-22 21:04:18 +0800

    更改了跳转链接

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="footer"> 学习养成记 ©2018 Created by lengdaner <a href="http://47.105.184.24:8088" target="_blank" style="color:#ccc;font-size:10px;line-height:10px;">管理员</a></div>
+  <div class="footer"> 学习养成记 ©2018 Created by lengdaner <a href="www.feiliu.site" target="_blank" style="color:#ccc;font-size:10px;line-height:10px;">管理员</a></div>
 </template>
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
